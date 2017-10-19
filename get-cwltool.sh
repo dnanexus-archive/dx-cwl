d6d6336198fc8076cb4ded00cb890e513f2a10c7
Geet Duggal <geetduggal@gmail.com>
2017-10-19 22:20:03 +0000

    Link to official cwltool repo instead of custom fork

--- get-cwltool.sh
+++ get-cwltool.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget https://dl.dnanex.us/F/D/gV3B3KVFF3BXgj8kz1479qYg52BPPF8zQ1YgXFBB/cwltool.tgz
-tar -xzf cwltool.tgz
+git clone https://github.com/common-workflow-language/cwltool.git
