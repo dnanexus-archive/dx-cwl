c543a8675e02fbbd035604d2ee5cb53c51be968f
Geet Duggal <geetduggal@gmail.com>
2017-11-10 23:23:21 +0000

    Early framework for conformance testing

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install -y wget git make python-setuptools python-pip python-virtualenv python-dev g++ cmake libboost1.55-all-dev libcurl4-openssl-dev zlib1g-dev libbz2-dev flex bison autoconf curl parallel
 
-pip install cwltool PyYAML
+pip install cwltool cwltest PyYAML
 
 # Obtain and install the latest stable toolkit
 git clone https://github.com/dnanexus/dx-toolkit.git
--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 dx login --token $DXTOKEN --noprojects
 dx new project -s cwltests
@@ -16,8 +17,8 @@ dx upload -r tests
 ./dx-cwl -h
 
 ## CORE INTEGRATION TESTS (eventually run GNU parallel)
-./dx-cwl compile-workflow tests/md5sum/md5sum.cwl --token $DXTOKEN --project $DXPROJ
-./dx-cwl run-workflow dx-cwl-run/md5sum/md5sum /tests/md5sum/md5sum.cwl.json --token $DXTOKEN --project $DXPROJ --wait
+#./dx-cwl compile-workflow tests/md5sum/md5sum.cwl --token $DXTOKEN --project $DXPROJ
+#./dx-cwl run-workflow dx-cwl-run/md5sum/md5sum /tests/md5sum/md5sum.cwl.json --token $DXTOKEN --project $DXPROJ --wait
 
 ## A bunch of example tests
 
@@ -25,6 +26,23 @@ dx upload -r tests
 
 ## CWL CONFORMANCE TESTS (after completing core integration tests)
 
-## CLEANUP
 
-dx rmproject -y $DXPROJ
+rm -rf common-workflow-language
+git clone https://github.com/common-workflow-language/common-workflow-language.git
+cd common-workflow-language
+dx upload -r v1.0/v1.0
+chmod 777 run_test.sh
+rm -f commands.txt
+for testnum in 21; do
+  echo "./run_test.sh -n${testnum} RUNNER=$DIR/dx-cwl-runner" >> commands.txt
+done
+
+parallel --joblog joblog.txt < commands.txt
+# Print results
+awk '$7 == 0 {print "PASSED " $0} $7 == 1 {print "FAILED " $0} NR == 1 {print "     " $0}' joblog.txt
+cd ..
+
+
+
+## CLEANUP
+#dx rmproject -y $DXPROJ
